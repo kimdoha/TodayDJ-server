98939e05d802603a9ffd3cc57ecd5900dce69341
Minji Kim <kkhhjj3393@gmail.com>
2021-05-28 01:29:17 +0900

    #bug

--- config/baseResponseStatus.js
+++ config/baseResponseStatus.js
@@ -1,43 +1,5 @@
 module.exports = {
 
-    // Success
-    SUCCESS : { "isSuccess": true, "code": 1000, "message":"성공" },
-
-    // Common
-    TOKEN_EMPTY : { "isSuccess": false, "code": 2000, "message":"JWT 토큰을 입력해주세." },
-    TOKEN_VERIFICATION_FAILURE : { "isSuccess": false, "code": 3000, "message":"JWT 토큰 검증 실패" },
-    TOKEN_VERIFICATION_SUCCESS : { "isSuccess": true, "code": 1001, "message":"JWT 토큰 검증 성공" }, // ?
-
-    //Request error
-    SIGNUP_EMAIL_EMPTY : { "isSuccess": false, "code": 2001, "message":"이메일을 입력해주세요" },
-    SIGNUP_EMAIL_LENGTH : { "isSuccess": false, "code": 2002, "message":"이메일은 30자리 미만으로 입력해주세요." },
-    SIGNUP_EMAIL_ERROR_TYPE : { "isSuccess": false, "code": 2003, "message":"이메일을 형식을 정확하게 입력해주세요." },
-    SIGNUP_PASSWORD_EMPTY : { "isSuccess": false, "code": 2004, "message": "비밀번호를 입력 해주세요." },
-    SIGNUP_PASSWORD_LENGTH : { "isSuccess": false, "code": 2005, "message":"비밀번호는 6~20자리를 입력해주세요." },
-    SIGNUP_NICKNAME_EMPTY : { "isSuccess": false, "code": 2006, "message":"닉네임을 입력 해주세요." },
-    SIGNUP_NICKNAME_LENGTH : { "isSuccess": false,"code": 2007,"message":"닉네임은 최대 20자리를 입력해주세요." },
-
-    SIGNIN_EMAIL_EMPTY : { "isSuccess": false, "code": 2008, "message":"이메일을 입력해주세요" },
-    SIGNIN_EMAIL_LENGTH : { "isSuccess": false, "code": 2009, "message":"이메일은 30자리 미만으로 입력해주세요." },
-    SIGNIN_EMAIL_ERROR_TYPE : { "isSuccess": false, "code": 2010, "message":"이메일을 형식을 정확하게 입력해주세요." },
-    SIGNIN_PASSWORD_EMPTY : { "isSuccess": false, "code": 2011, "message": "비밀번호를 입력 해주세요." },
-
-    USER_USERID_EMPTY : { "isSuccess": false, "code": 2012, "message": "userId를 입력해주세요." },
-    USER_USERID_NOT_EXIST : { "isSuccess": false, "code": 2013, "message": "해당 회원이 존재하지 않습니다." },
-
-    USER_USEREMAIL_EMPTY : { "isSuccess": false, "code": 2014, "message": "이메일을 입력해주세요." },
-    USER_USEREMAIL_NOT_EXIST : { "isSuccess": false, "code": 2015, "message": "해당 이메일을 가진 회원이 존재하지 않습니다." },
-    USER_ID_NOT_MATCH : { "isSuccess": false, "code": 2016, "message": "유저 아이디 값을 확인해주세요" },
-    USER_NICKNAME_EMPTY : { "isSuccess": false, "code": 2017, "message": "변경할 닉네임 값을 입력해주세요" },
-
-    USER_STATUS_EMPTY : { "isSuccess": false, "code": 2018, "message": "회원 상태값을 입력해주세요" },
-
-    // folder
-    FOLDER_ID_EMPTY : { "isSuccess": false, "code": 2019, "message": "folder ID를 입력해주세요" },
-    FOLDER_NAME_EMPTY : { "isSuccess": false, "code": 2020, "message": "folder 이름을 입력해주세요" },
-    INPUT_NUMBER : { "isSuccess": false, "code": 2021, "message": "숫자로 입력해주세요" },
-    SET_FOLDER_NAME : { "isSuccess": false, "code": 2022, "message": "폴더 이름 3~20자로 입력해주세요" },
-
     // feeling
     FEELING_EMPTY : { "isSuccess": false, "code": 2023, "message": "기분 인덱스를 입력해주세요" },
     FEELING_ID_EXCEED : { "isSuccess": false, "code": 2024, "message": "기분 인덱스 범위를 벗어났습니다" },
@@ -84,9 +46,7 @@ module.exports = {
     EMPTY_PLAYLIST2_RESULT : { "isSuccess": false, "code": 3013, "message": "해당 날씨의 추천된 노래가 없습니다." },
     EMPTY_PLAYLIST_RESULT : { "isSuccess": false, "code": 3014, "message": "좋아요 누른 노래가 없습니다." },
 
-    //Connection, Transaction 등의 서버 오류
     DB_ERROR : { "isSuccess": false, "code": 4000, "message": "데이터 베이스 에러"},
     SERVER_ERROR : { "isSuccess": false, "code": 4001, "message": "서버 에러"},
- 
  
 }
